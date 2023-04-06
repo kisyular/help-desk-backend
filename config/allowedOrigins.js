c59fbf35a64e2c42c3d28468379005665484e835
kisyular <rellika@kisyula.com>
2023-04-06 16:10:31 +0300

    A NODE EXPRESS SERVER - MIDDLE WARE CORS AND EVENT LOGGING AND ERROR LOGGING

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+	'http://localhost:3000',
+	'https://www.google.com',
+	'https://google.com',
+	'https://www.kisyula.com',
+	'https://kisyula.com',
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const corsOptions = require('./config/corsOptions')
+
+// Middleware for logging requests
+app.use(logger)
+
+// Middleware for parsing cookies
+app.use(cookieParser())
+
+// Middleware for parsing JSON
+app.use(express.json())
+
+// Middleware for parsing form data
+app.use(express.urlencoded({ extended: true }))
+
+// Middleware for CORS
+app.use(cors(corsOptions))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
@@ -16,5 +36,8 @@ app.all('*', (req, res) => {
 		res.type('txt').send('404 Not Found')
 	}
 })
+
+// Middleware for error handling
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
