7ba1b3ba6ae5c9d8bbdcf573c851dee8660dbb4b
kisyular <rellika@kisyula.com>
2023-04-10 15:04:20 +0300

    HANDLING CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
 	'https://www.kisyula.com',
 	'https://kisyula.com',
 	'https://helpdesk-v6b2.onrender.com',
+	'http://localhost:3000/',
 ]
 
 module.exports = allowedOrigins
