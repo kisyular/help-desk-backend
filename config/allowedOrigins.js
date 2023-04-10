548dc170f8c376a29bbe98c5fe269b559aa7c54c
kisyular <rellika@kisyula.com>
2023-04-10 03:28:27 +0300

    BACKEND END - REFACTORING - LIVE PRODUCTION

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,4 @@
 const allowedOrigins = [
-	'http://localhost:3000',
-	'https://www.google.com',
-	'https://google.com',
 	'https://www.kisyula.com',
 	'https://kisyula.com',
 	'khelpdesk.onrender.com',
